fb015dcd933d1101e24b8074325bd9c81b413494
dani <danifromecuador@gmail.com>
2024-06-04 23:47:47 -0500

    Add a checked property to each miniBlock | feat/layout | src/day/Day.jsx

--- src/day/Day.jsx
+++ src/day/Day.jsx
@@ -2,30 +2,35 @@ import { useState } from 'react'
 import './Day.css'
 
 export const Day = ({ blocks = [], label }) => {
+  let blocksWithChecked = []
+  for (let i = 0; i < blocks.length; i++) {
+    let miniBlock = []
+    for (let j = 0; j < blocks[i].duration + 1; j++) {
+      miniBlock.push({ completed: "false" })
+    }
+    blocksWithChecked.push({ miniBlock: miniBlock, durationClass: blocks[i].durationClass })
+  }
+
   const [workedHours, setWorkedHours] = useState(0)
   const handleCheckBoxChange = (event) => {
-    
-    if (event.target.checked === true) {
-      setWorkedHours(workedHours + 1)
-    }
-    else setWorkedHours(workedHours - 1)
+    event.target.checked === true ? setWorkedHours(workedHours + 1) : setWorkedHours(workedHours - 1)
   }
 
   return (
     <div className='Header Day'>
       <div className="blocks">
-        {blocks.map((block, index) => (
-          <div className='block-label' key={index}>
+        {blocksWithChecked.map((block, indexBlock) => (
+          <div className='block-label' key={indexBlock} >
             <div className={`block ${block.durationClass}`}>
-              {Array(block.duration + 1).fill().map((_, index) => (
-                <input type="checkbox" key={index} className='mini-block' onChange={handleCheckBoxChange} />
+              {block.miniBlock.map((miniBlock, indexMiniBlock) => (
+                <input type="checkbox" key={indexMiniBlock} className='mini-block' onChange={handleCheckBoxChange} />
               ))}
             </div>
           </div>
         ))}
       </div>
       <div className="worked-hours">
-        {label}: {workedHours/4} h
+        {label}: {workedHours / 4} h
       </div>
     </div>
   )
