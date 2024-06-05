528e9f3294c56ca9a4715a68d5d9bb287602913f
dani <danifromecuador@gmail.com>
2024-06-05 15:13:30 -0500

    Create a Day component that renders day array from store | feat/days | src/day/ |

--- src/day/Day.jsx
+++ src/day/Day.jsx
@@ -1,10 +1,39 @@
+import { useEffect } from 'react'
+import { Store } from '../store/store'
 import './Day.css'
 
 export const Day = () => {
+  const store = Store()
+  useEffect(() => store.createDays(), [store.blocks])
 
   return (
-    <div className='Day'>
-      day
+    <div className='Header Day'>
+      {
+        store.blocks.length >= 1 &&
+        store.days.map((day) => (
+          <div className='day'>
+            <div className='blocks'>
+              {day.blocks.map((block, indexBlock) => (
+                <div className='block-and-name' key={indexBlock}>
+                  <div className='block' onClick={() => addMiniBlock(indexBlock)}>
+                    {block.miniBlocks.map((miniBlock, indexMiniBlock) => (
+                      <div
+                        className='mini-block'
+                        key={indexMiniBlock}
+                        onContextMenu={(event) => deleteMiniBlock(event, indexBlock)}
+                      >
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              ))}
+            </div>
+            <div>
+              {day.name}
+            </div>
+          </div>
+        ))
+      }
     </div>
   )
 }
