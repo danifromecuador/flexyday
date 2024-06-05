fbfb7d40a5d838152051ffa32cb43c0279359e0a
dani <danifromecuador@gmail.com>
2024-06-05 11:20:22 -0500

    Add a new block in store | feat/layout | src/ |

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -1,9 +1,36 @@
+import { useState } from 'react'
+import { Store } from '../store/store.js'
 import './Header.css'
 
-export const Header = ({ blocksToParent }) => {
+export const Header = () => {
+  const store = Store()
+  const [input, setInput] = useState("")
+  const [placeHolder, setPlaceHolder] = useState("Type a new block and press Enter")
+
+  const handleEnter = (e) => {
+    if (e.key === "Enter" && !input) setPlaceHolder("Type something")
+    if (e.key === "Enter" && input) {
+      console.log(input);
+      store.addBlock(input)
+      setInput("")
+      setPlaceHolder("Type a new block and press Enter")
+    }
+  }
+
   return (
     <div className='Header'>
-      header
+      <input
+        type="text"
+        placeholder={placeHolder}
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        onKeyDown={(e) => handleEnter(e)}
+      />
+      <div className="blocks">
+        {store.blocks.map(block => (
+          <div className='block'></div>
+        ))}
+      </div>
     </div>
   )
 }
---
+++ src/store/store.js
@@ -0,0 +1,10 @@
+import { create } from "zustand"
+import { devtools } from "zustand/middleware"
+
+export const Store = create(devtools((set) => ({
+  bears: 23,
+  blocks: [],
+  addBlock: (name) => set((state) => ({
+    blocks: [...state.blocks, {name:name, miniblocks:[{completed:false}]}]
+  }))
+})))
