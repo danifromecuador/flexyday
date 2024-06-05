484623350f63712155007e2a7866ddc1687f5157
dani <danifromecuador@gmail.com>
2024-06-04 19:24:47 -0500

    Increase block duration | feat/layout | src/header/Header.jsx |

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -3,19 +3,28 @@ import './Header.css'
 
 export const Header = () => {
   const [blockName, setBlockName] = useState("")
-  // const [blockDuration, setBlockDuration] = useState("")
+  const [blockDurationClass, setBlockDurationClass] = useState("a")
   const [blocks, setBlocks] = useState(JSON.parse(localStorage.getItem("blocks")) || [])
+  const alphabet = [..."abcdefghijk"]
 
   const handleKeyDown = (e) => {
     if (e.key === "Enter") {
-      setBlocks([...blocks, { name: blockName, duration: 1 }])
+      setBlocks([...blocks, { name: blockName, duration: 1, durationClass:alphabet[1] }])
       setBlockName("")
-      // setBlockDuration("")
     }
   }
 
+  const increaseBlockDuration = (i) => {
+    const newBlocks = [...blocks];
+    newBlocks[i] = { ...newBlocks[i], duration: newBlocks[i].duration + 1, durationClass: alphabet[newBlocks[i].duration] };
+    setBlocks(newBlocks);
+  };
+
   useEffect(() => {
     localStorage.setItem("blocks", JSON.stringify(blocks))
+
+    console.log(blocks);
+
   }, [blocks])
 
   return (
@@ -28,11 +37,17 @@ export const Header = () => {
           onChange={(e) => setBlockName(e.target.value)}
           onKeyDown={(e) => handleKeyDown(e)}
         />
-        <button onClick={()=>setBlocks([])}>Delete All Blocks</button>
+        <button onClick={() => setBlocks([])}>Delete All Blocks</button>
       </div>
       <div className="blocks">
-        {blocks.map(block => (
-          <div className={`block ${block.duration}`}>{block.name}</div>
+        {blocks.map((block, index) => (
+          <div
+            className={`block ${block.durationClass}`}
+            onClick={() => increaseBlockDuration(index)}
+          >
+            {block.name}
+            {block.duration}
+          </div>
         ))}
       </div>
     </div>
